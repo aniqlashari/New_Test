8a609cf71edddd5ea28b341cf0a404944dbad332
aniqlashari <mahaqamber3@gmail.com>
2024-10-17 20:17:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// let date = new Date();
+// let dates = ["Sun","mon","Tues","Wed","Thurs","Fri","Sat"]
+
+
+// let day = date.getDay();
+
+// console.log(dates[day])
+
+
+
+
+// function goli(){
+//    for (let index = 1; index <= 1; index++) {
+   
+    
+   
+//     let telltime = new Date()
+//    let time = telltime.getHours()
+//    let time1= telltime.getMinutes()
+
+ 
+//     console.log(time +":"+ time1);
+    
+// }}
+
+// let num = prompt("Gess Any Number")
+// let num1= Math.round((nam * 7)+1)
+// console.log(num1)
+
+// let num2= prompt("gess any number")
+
+let num = Math.floor(Math.random() * 1000000) + 1;
+ console.log(num);
+ 
+
+let pro = prompt("Guess a number ");
+if (parseInt(pro) === num) {
+    alert("O bhi right! ✌️");
+} else {
+    alert("Nhi Nhi YOur Luck IS 🤮  number 👉 " + num + "  .");
+}
